095f79d2877a7c7712901fac16c1bf1bf06f8711
Kollawach Satiansareesuk <kollawach.s@ku.th>
2021-08-01 03:33:54 +0700

    Add Login_Page and Register_Page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -13,12 +9,15 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  min-height: 100vh;
 }
-
+body{
+    margin: 0px;
+  }
 #nav {
-  padding: 30px;
+  // padding: 30px;
+  
 
   a {
     font-weight: bold;
@@ -28,5 +27,6 @@
       color: #42b983;
     }
   }
+
 }
 </style>
