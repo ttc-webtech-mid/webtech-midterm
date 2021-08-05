05f0cfab4180bd6a6cc558059d0e6366024ee0f2
Patipan <patipan.bo@ku.th>
2021-08-06 03:08:12 +0700

    import tailwind css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 import store from './store'
+import '../index.css'
 
 Vue.use(BootstrapVue)
 
