fda1507e00bf17dbfc10e7a33845ecd5372c9acb
Kollawach Satiansareesuk <kollawach.s@ku.th>
2021-08-09 16:52:46 +0700

    Update Home_Page of admin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,61 @@
       <div class="content_wrapper">
         <Sidebar page="Home"></Sidebar>
         <div class="content_pad">
-        <course-list></course-list>
+        <!-- <course-list></course-list> -->
+
+          <div class="board_wrapper">
+            
+            <div class="head_table">
+              <span id="header_table">POINTS EARNED</span>
+              <input id="time_search" type="date">
+              <span id="time_search">Search :</span>
+            </div>
+
+            <table>
+                <thead>
+                    <tr id="head">
+                        <th id="rank_head">Rank</th>
+                        <th id="name_head">Name</th>
+                        <th id="points_head">Total Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(std, index) in students" :key="index">
+                        <td id="rank_body"> {{ index + 1 }} </td>
+                        <td id="name_body"> {{ `${std.firstname} ${std.lastname}` }} </td>
+                        <td id="points_body">{{ `${std.scores}` }}</td>
+                    </tr>
+                </tbody>
+            </table>  
+          </div>
+
+          <div class="board_wrapper">
+
+            <div class="head_table">
+              <span id="header_table">POINTS USED</span>
+              <input id="time_search" type="date">
+              <span id="time_search">Search :</span>
+            </div>
+
+              <table>
+                  <thead>
+                      <tr id="head">
+                          <th id="rank_head">Rank</th>
+                          <th id="name_head">Name</th>
+                          <th id="points_head">Total Points</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr v-for="(std, index) in students" :key="index">
+                          <td id="rank_body"> {{ index + 1 }} </td>
+                          <td id="name_body"> {{ `${std.firstname} ${std.lastname}` }} </td>
+                          <td id="points_body">{{ `${std.scores}` }}</td>
+                      </tr>
+                  </tbody>
+              </table>
+              
+          </div>
+
         </div>
       </div>
   </div>
@@ -61,5 +115,70 @@ export default {
       }
     }
 }
+
+.board_wrapper{
+        display: block;
+        background-color: #fff;
+        margin:45px auto;
+        width: 1000px;
+        height: 90%;
+        overflow-y: scroll;
+        
+        .head_table{
+          color: #fff;
+          display: block;
+          width: 995px;
+          height: 50px;
+          background-color: #13516b;
+        }
+        #header_table{
+          display: block;
+          float: left;
+          margin: 13px 0px 0px 20px;
+          font-weight: bold;
+        }
+        #time_search{
+          display: block;
+          float: right;
+          margin: 12px 20px 0px 0px;
+        }
+        #head{
+            background-color: #327997;
+            color: #fff;
+            height: 50px;
+            text-align: center;
+            
+            #rank_head{
+                width: 150px;
+                border-right: 2px solid #ffffff;
+            } 
+            #points_head{
+                width: 150px;
+                border-left: 2px solid #ffffff;
+            }
+            #name_head{
+                width: 700px;
+            }
+        }
+        tbody{
+            tr{
+                height: 50px;
+              }
+            
+            #rank_body{
+                text-align:center;
+                // border-right: 2px solid #000000;
+            }
+            #points_body{
+                // border-left: 2px solid #000000;
+                text-align:center;
+            }
+            #name_body{
+                
+                display: block;
+                margin:20px 20px;
+            }
+        }
+      }
 
 </style>
