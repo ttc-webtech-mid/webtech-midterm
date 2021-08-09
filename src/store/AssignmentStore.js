606c7b36caeac5af55fc35e6d4cdf59b3a138563
Patipan <patipan.bo@ku.th>
2021-08-09 17:12:28 +0700

    add role teacher

--- src/store/AssignmentStore.js
+++ src/store/AssignmentStore.js
@@ -9,12 +9,14 @@ export default new Vuex.Store({
   state: {
       assignments: [],
       filterAssignment: [],
-      uploadedFiles: []
+      uploadedFiles: [],
+      turnedIn: []
   },
   getters: {
       getAssignments: (state) => state.assignments,
       getFilterAssignment: (state) => state.filterAssignment,
-      getUploadedFiles: (state) => state.uploadedFiles
+      getUploadedFiles: (state) => state.uploadedFiles,
+      getTurnedIn: (state) => state.turnedIn
   },
   mutations: {
       setAssignments(state, res){
@@ -22,28 +24,52 @@ export default new Vuex.Store({
       },
       setFilterAssignment(state, res){
           state.filterAssignment = res.data
-          console.log(state.filterAssignment)
       },
       setUploadedFiles(state) {
           let handInAssignments = state.filterAssignment[0]['hand_in_assignments']
           let uploadedFiles = handInAssignments.filter(task => task.student === 2)
           state.uploadedFiles = uploadedFiles
+      },
+      addAssignment(state, payload) {
+        state.assignments.push(payload.data)
+      },
+      setTurnedIn(state, res) {
+          state.turnedIn = res
       }
   },
   actions: {
       async fetchAssignments({ commit }){
-
+        let res = await axios.get(baseURL + "/assignments")
+        commit('setAssignments', res)
       },
-      async fetchAssignmentById({ commit }, task_id){
+      async fetchAssignmentById({ commit }, {task_id, role}){
           let res = await axios.get(baseURL + "/assignments?id=" + task_id)
-          commit('setFilterAssignment', res)
+          if(role === 'Teacher') {
+              let { hand_in_assignments, students } = res.data[0]
+              let bindingData = []
+              for(let assignment of hand_in_assignments) {
+                  for(let student of students) {
+                      if(assignment.student === student.id) {
+                          bindingData.push({
+                              assignment: assignment,
+                              student: student
+                          })
+                      }
+                  }
+              }
+
+              commit('setFilterAssignment', res)
+              commit('setTurnedIn', bindingData)
+          }else {
+              commit('setFilterAssignment', res)
+          }
       },
       fetchUploadedFiles({ commit }) {
           commit('setUploadedFiles')
       },
       async uploadAssignments({ commit }, formData) {
             /*
-                Request body: {
+                Request body {
                     student: id
                     assignment: id
                     files: []
@@ -64,45 +90,31 @@ export default new Vuex.Store({
             } catch(e) {
                 console.error(e)
             }
-      }
-  },
-  modules: {
-  }
-})
-
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
-
-let baseURL = process.env.VUE_APP_API_ENDPOINT || "http://localhost:1337"
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-      assignments: []
-  },
-  getters: {
-      getAssignments: (state) => state.assignments
-  },
-  mutations: {
-      setAssignments(state, res){
-        state.assignments = res.data
       },
-      addAssignment(state, payload) {
-        state.courses.push(payload)
-      }
-  },
-  actions: {
-      async fetchAssignments({ commit }) {
-        let res = await axios.get(baseURL + "/assignments")
-        commit('setAssignments', res)
-      },
-      async addAssignment({ commit }, payload) {
+       async addAssignment({ commit }, payload) {
+           /*
+           REQUEST BODY
+           {
+                "topic": "test", yes
+                "detail": "test",yes,
+                "due_date": yes
+                "course": 1,
+                "teacher": [4],
+                "students": [1, 2]
+            }
+           */
+        let student = await axios.get(`${baseURL}/students?courses.course_id=${payload.courseID}`)
+        let course = await axios.get(`${baseURL}/courses?course_id=${payload.courseID}`)
+        console.log(course.data)
         let body = {
             topic: payload.topic,
             detail: payload.detail,
-            due_date: payload.due_date
+            due_date: payload.due_date,
+            course: course.data[0].id,
+            teacher: payload.teacher,
+            students: student.data.map(std => std.id)
         }
+
         let url = baseURL + `/assignments/`
         let res = await axios.post(url, body)
         commit('addAssignment', res)
