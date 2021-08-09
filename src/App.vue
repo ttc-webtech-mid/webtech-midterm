75984a5929db35e9b4caaa17951be4d90fabb4fc
Patipan <patipan.bo@ku.th>
2021-08-10 06:52:08 +0700

    import font

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Kanit&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, Kanit, sans-serif;
   background-color: #EAEAEA;
 }
 body{
